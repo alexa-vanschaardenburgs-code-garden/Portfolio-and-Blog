d2ee781662611fd5e9f5834a9ab80f8e88772857
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-09-12 12:21:58 -0600

    feat: Add flower images for About page

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,9 +1,10 @@
 import '../Nav/Nav.scss';
+import { NavLink } from 'react-router-dom'
 
 function Nav() {
   return (
     <div className='nav'>
-      <img className="nav-logo" src={`${process.env.PUBLIC_URL}/logo_nav.png`}/>
+      <NavLink className="nav-logo" to="/"><img className="nav-logo" src={`${process.env.PUBLIC_URL}/logo_nav.png`}/></NavLink>
       <button className="menu-button"><span className="material-symbols-rounded menu-button">menu</span></button>
     </div>
   );
