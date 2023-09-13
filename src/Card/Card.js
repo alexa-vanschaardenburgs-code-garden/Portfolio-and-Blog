ae6c74a14657b6590bfe0f52fd36437207043ac9
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-09-13 15:07:58 -0600

    feat: Build Card component

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,11 +1,27 @@
 import '../Card/Card.scss';
+import { NavLink } from 'react-router-dom';
 
 function Card({ project }) {
+
+  const createBullets = (words) => {
+    return (
+      <div className="bullet">
+        <img className="point-flower" src={`${process.env.PUBLIC_URL}/point-flower.png`} />
+        <p>{words}</p>
+      </div>
+    )
+  }
+
   return (
-    <div>
-      <p>Card</p>
+    <div className="card-view">
       <div className="preview-img">
-      <img className="preview-img" src={`${process.env.PUBLIC_URL}${project.gif}`} />
+      <img className="project-gif" src={`${process.env.PUBLIC_URL}${project.gif}`} />
+      </div>
+      <div className="project-preview">
+        <h2>{project.name}</h2>
+        <p>{project["breif_summary"]}</p>
+        {project.highlights.map(highlight => createBullets(highlight))}
+        <NavLink to={'/'+project.id}>MORE DETAILS</NavLink>
       </div>
     </div>
   );
