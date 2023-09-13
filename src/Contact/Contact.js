7a7c8a13254e98f6e3ec100368d619f1f6a0b140
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-09-13 17:33:32 -0600

    feat: Create and style contact page

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,9 +1,19 @@
 import '../Contact/Contact.scss';
+import PageHeader from '../PageHeader/PageHeader.js';
 
 function Contact() {
   return (
-    <div>
-      <p>Contact</p>
+    <div className='contact'>
+      <PageHeader page='CONTACT'/>
+      <div className='email-container'>
+        <img className="point-flower" src={`${process.env.PUBLIC_URL}/point-flower.png`} />
+        <p>Email me at alexa.vsdb@gmail.com</p>
+        <img className="point-flower" src={`${process.env.PUBLIC_URL}/point-flower.png`} />
+      </div>
+      <div className='link-container'>
+        <a className="button" href="">Linkedin</a>
+        <a className="button" href="">GitHub</a>
+      </div>
     </div>
   );
 }
