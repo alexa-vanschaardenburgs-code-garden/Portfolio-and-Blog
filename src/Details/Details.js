b73a1b67b5b57b3e582d5b256e3d298194fe7bf7
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-09-13 17:06:44 -0600

    feat: Style the details page

--- src/Details/Details.js
+++ src/Details/Details.js
@@ -8,39 +8,35 @@ function Details() {
   let id = useParams()
   let project = projects.find(project => project.id === id.id)
 
+  const displaySectionHeader = (title) => {
+    return (
+      <div className="detail-section-header">
+      <img className="point-flower" src={`${process.env.PUBLIC_URL}/point-flower.png`} />
+      <h2>{title}</h2>
+      <img className="point-flower" src={`${process.env.PUBLIC_URL}/point-flower.png`} />
+    </div>
+    )
+  }
+
   return (
-    <div className="details">
+    <div className="details-container">
       <PageHeader page={project.name}/>
-      <img className="project-gif" id="details-gif" src={`${process.env.PUBLIC_URL}${project.gif}`} />
-      <div className="links-container">
-        <button className="button detail-button" to="/about-me">LEARN MORE ABOUT ME</button>
-        <button className="button detail-button" to="/work">SEE MORE OF MY WORK</button>
-      </div>
-      <div className="detail-section-header">
-        <img className="point-flower" src={`${process.env.PUBLIC_URL}/point-flower.png`} />
-        <h2>CONTEXT</h2>
-        <img className="point-flower" src={`${process.env.PUBLIC_URL}/point-flower.png`} />
-      </div>
-      <p>{project.context}</p>
-      <div className="detail-section-header">
-        <img className="point-flower" src={`${process.env.PUBLIC_URL}/point-flower.png`} />
-        <h2>TECH STACK</h2>
-        <img className="point-flower" src={`${process.env.PUBLIC_URL}/point-flower.png`} />
-      </div>
-      <p>{project.techs.join(" | ")}</p>
-      <div className="detail-section-header">
-        <img className="point-flower" src={`${process.env.PUBLIC_URL}/point-flower.png`} />
-        <h2>SUMMARY</h2>
-        <img className="point-flower" src={`${process.env.PUBLIC_URL}/point-flower.png`} />
-      </div>
-      <p>{project.summary}</p>
-      <div className="detail-section-header">
-        <img className="point-flower" src={`${process.env.PUBLIC_URL}/point-flower.png`} />
-        <h2>PREVIEW</h2>
-        <img className="point-flower" src={`${process.env.PUBLIC_URL}/point-flower.png`} />
-      </div>
-      <div className="detail-img-container">
-        {project.imgs.map(img => <img className="detail-img" src={`${process.env.PUBLIC_URL}${img}`} />)}
+      <div className="details">
+        <img className="project-gif" id="details-gif" src={`${process.env.PUBLIC_URL}${project.gif}`} />
+        <div className="links-container">
+          <button className="button detail-button" to="/about-me">VIST REPO</button>
+          <button className="button detail-button" to="/work">VISIT SITE</button>
+        </div>
+        {displaySectionHeader('CONTEXT')}
+        <p>{project.context}</p>
+        {displaySectionHeader('TECH STACK')}
+        <p>{project.techs.join(" | ")}</p>
+        {displaySectionHeader('SUMMARY')}
+        <p>{project.summary}</p>
+        {displaySectionHeader('STILL SHOTS')}
+        <div className="detail-img-container">
+          {project.imgs.map(img => <img className="detail-img" src={`${process.env.PUBLIC_URL}${img}`} />)}
+        </div>
       </div>
     </div>
   );
