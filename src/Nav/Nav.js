3c41ce655466f8ab8c95db61eb7a8c4f2e6dbbb5
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-09-13 18:00:24 -0600

    feat: Add nav 'page' over all other pages when user clicks onmenu

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -4,14 +4,18 @@ import { useState } from 'react'
 
 function Nav() {
 
-  const [navExpanded, setNavExpanded] = useState(false)
+  const [navExpanded, setNavExpanded] = useState(true)
 
   const displayNavButtons = () => {
     console.log("click heard")
     return (
-      <>
-        <p>wheeeeee!</p>
-      </>
+      <div className='dropdown'>
+        <button className="menu-button toggle-off" onClick={() => {setNavExpanded(!navExpanded)}}><span className="material-symbols-rounded menu-button">menu</span></button>
+        <NavLink to="/" onClick={() => {setNavExpanded(!navExpanded)}} className="nav-location">HOME<img className="point-flower" src={`${process.env.PUBLIC_URL}/point-flower.png`} /></NavLink>
+        <NavLink to="/about-me" onClick={() => {setNavExpanded(!navExpanded)}} className="nav-location">ABOUT ME<img className="point-flower" src={`${process.env.PUBLIC_URL}/point-flower.png`} /></NavLink>
+        <NavLink to="work" onClick={() => {setNavExpanded(!navExpanded)}} className="nav-location">MY WORK<img className="point-flower" src={`${process.env.PUBLIC_URL}/point-flower.png`} /></NavLink>
+        <NavLink to="/contact" onClick={() => {setNavExpanded(!navExpanded)}} className="nav-location">CONTACT<img className="point-flower" src={`${process.env.PUBLIC_URL}/point-flower.png`} /></NavLink>
+      </div>
     )
   }
 
